f3516110dc59a19598203183b5f43936248c5d0d
Survy Vaish <sarvagya.vaish7@gmail.com>
2025-05-19 18:36:27 -0700

    Switch to YOLO11

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
   const canvasRef = useRef(null);
 
   // model configs
-  const modelName = "yolov8n";
+  const modelName = "yolo11n";
 
   useEffect(() => {
     tf.ready().then(async () => {
